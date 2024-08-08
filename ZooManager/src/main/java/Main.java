5ffdc01ce1318ff840b78bc9db0299477f56a496
grmvap <137700601+grmvap@users.noreply.github.com>
2024-08-08 17:46:47 +0200

    Main.java

--- ZooManager/src/main/java/Main.java
+++ ZooManager/src/main/java/Main.java
@@ -9,20 +9,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args)  {
 
-
-        /*Wolf wolf = new Wolf("Волк", 4, true,"Серый", "Сибирь");
-        Tiger tiger = new Tiger("Тигр", 4,true, "Желиый", "Африка");
-        testPolimorf(tiger);
-        testPolimorf(wolf);
-        List <Animal> list = new ArrayList<>();
-        list.add(tiger);
-        list.add(wolf);
-        for (int i = 0; i < list.size(); i++) {
-           testPolimorf(list.get(i));
-        }*/
-
-        //System.out.println("Введите: 'создать тигра'");
-
         AnimalCrudService animalCrudService = new AnimalCrudService();
 
         CommandController commandController = new CommandController(animalCrudService);
@@ -30,11 +16,7 @@ public class Main {
 
 
 
-    }
-
-    public static void testPolimorf(Animal animal) {
-        System.out.println(animal.toString());
-    }
+    
 
 
 }
